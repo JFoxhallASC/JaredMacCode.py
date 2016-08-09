c17dad1dc0b3426ebce4ff4e4072ef6fb571b56c
JFoxhall <jfoxhall99@gmail.com>
2016-08-09 10:31:18 -0400

    Link to firebase

---
+++ Database Code/jaredDatabase.js
@@ -0,0 +1,19 @@
+
+
+var database = firebase.database().ref(); //link to our database
+
+
+//for sending data to the database
+function sendMessage(){
+  var name = $("#name").val();
+  var message = $("#message").val();
+  database.push({
+    'NAME':name,
+    'MESSAGE':message
+  });
+}
+
+database.on('child_added', function(dataRow){
+  var row = dataRow.val();
+  $("#messageBoard").append("<p>"+row.NAME+": " + row.MESSAGE);
+})
